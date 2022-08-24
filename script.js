d1d5ba6c027ffdada4dc06a306e671b9efe14a79
Yuri Guilherme Santana de Sá <yuri.luana2016@gmail.com>
2022-08-24 17:27:19 -0300

    Configure input:text color interaction

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+/* Atribuição de elementos */
 const bgCard = document.querySelector(".bg-card");
 const button  = document.getElementById("btn");
 const textInput = document.getElementById("colorText")
+const textInputLabel = document.getElementById("colorText_label")
+const hexError = document.getElementById("hexError")
 
+/* Função para atribuição de estilos a elementos */
 const getStyle = (element, style) => {
     window
     .getComputedStyle(element)
@@ -9,9 +13,10 @@ const getStyle = (element, style) => {
 }
 var bg = getStyle(bgCard, "--bg");
 
+/* Base de cores para a Função de troca de cores por clique; */
 const colors = [ "#000", "#FA2", "#E3C", "#1FBB"]
 
-
+/* Função de troca de cores pelo clique */
 let clickColor = () => {
     if (bg == undefined || bg == colors[0]) {
         bg = colors[1]
@@ -27,10 +32,22 @@ let clickColor = () => {
     bgCard.style.setProperty("--bg", bg)
 }
 
+let verificaHex = () => {
+    if((textInput.value.length != 3) && (textInput.value.length != 6) && (textInput.value.length != 8)) {
+        /* console.log("Inválido") */
+        hexError.style.setProperty("display", "flex");
 
+    } else {
+        /* console.log("Válido"); */
+        hexError.style.setProperty("display", "none")
+    }
+}
+/* Função de troca de cores pelo textInput */
 let writeColor = () => {
     bg = textInput.value
     bgCard.style.setProperty("--bg", `#${bg}`)
 }
+/* EventListeners */
 button.addEventListener("click", clickColor)
 textInput.addEventListener("keyup", writeColor)
+textInput.addEventListener("keyup", verificaHex)
